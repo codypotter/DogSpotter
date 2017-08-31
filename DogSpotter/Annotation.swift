6283df90bb15231634e8a333ca20f184d4427924
Cody Potter <codypotter@codys-mac.local>
2017-08-30 20:51:46 -0700

    in a working state finally

---
+++ DogSpotter/Annotation.swift
@@ -0,0 +1,25 @@
+//
+//  Annotation.swift
+//  DogSpotter
+//
+//  Created by Cody Potter on 8/30/17.
+//  Copyright © 2017 Cody Potter. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Annotation: NSObject, MKAnnotation {
+    dynamic var coordinate : CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(location coord:CLLocationCoordinate2D) {
+        self.coordinate = coord
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
