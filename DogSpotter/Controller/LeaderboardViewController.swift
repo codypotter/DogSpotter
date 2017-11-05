a0b8fc79d225ececddb9e23dd2979852a3705b9b
CodyPotter <enjoipotter@gmail.com>
2017-11-04 23:56:06 -0700

    leaderboard is working

--- DogSpotter/Controller/LeaderboardViewController.swift
+++ DogSpotter/Controller/LeaderboardViewController.swift
@@ -23,8 +23,6 @@ class LeaderboardViewController: UIViewController, UITableViewDataSource, UITabl
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var myFollowersStringsArray = [String]()
-
         
         leaderboardTableView.delegate = self
         leaderboardTableView.dataSource = self
@@ -33,31 +31,57 @@ class LeaderboardViewController: UIViewController, UITableViewDataSource, UITabl
             userUID = Auth.auth().currentUser?.uid
             ref = Database.database().reference().child("users")
             
-            ref?.child(userUID!).child("following").observe(.childAdded, with: { (snapshot) in
+            ref?.child(userUID!).child("following").queryOrdered(byChild: "reputation").observe(.childAdded, with: { (snapshot) in
                 self.ref?.child(snapshot.key).observeSingleEvent(of: .value, with: { (snap) in
                     let userToAdd = User()
                     let userDict = snap.value as? [String : AnyObject] ?? [:]
                     
                     userToAdd.username = userDict["username"] as? String
-                    userToAdd.reputation = userDict["reputation"] as? Int
+                    userToAdd.reputation = Int((userDict["reputation"] as? String)!)
                     userToAdd.uid = userDict["uid"] as? String
-                    self.myFollowingUsersArray.append(userToAdd)
+                    self.myFollowingUsersArray.insert(userToAdd, at: 0)
                     self.leaderboardTableView.reloadData()
                 })
             })
-            ref?.child(userUID!).child("followers").observe(.childAdded, with: { (snapshot) in
+            ref?.child(userUID!).child("followers").queryOrdered(byChild: "reputation").observe(.childAdded, with: { (snapshot) in
                 self.ref?.child(snapshot.key).observeSingleEvent(of: .value, with: { (snap) in
                     let userToAdd = User()
                     let userDict = snap.value as? [String : AnyObject] ?? [:]
                     
                     userToAdd.username = userDict["username"] as? String
-                    userToAdd.reputation = userDict["reputation"] as? Int
+                    userToAdd.reputation = Int((userDict["reputation"] as? String)!)
                     userToAdd.uid = userDict["uid"] as? String
-                    self.myFollowersUsersArray.append(userToAdd)
+                    self.myFollowersUsersArray.insert(userToAdd, at: 0)
                     self.leaderboardTableView.reloadData()
+                    
+                    switch self.folllowersFollowingSegmentedControl.selectedSegmentIndex {
+                    case 0:
+                        if self.myFollowersUsersArray.indices.contains(0) {
+                            self.topUserLabel.text = "\(String(describing: self.myFollowersUsersArray[0].username!))\n\(String(describing: self.myFollowersUsersArray[0].reputation!))"
+                        }
+                        if self.myFollowersUsersArray.indices.contains(1) {
+                            self.secondUserLabel.text = "\(String(describing: self.myFollowersUsersArray[1].username!))\n\(String(describing: self.myFollowersUsersArray[1].reputation!))"
+                        }
+                        if self.myFollowersUsersArray.indices.contains(2) {
+                            self.thirdUserLabel.text = "\(String(describing: self.myFollowersUsersArray[2].username!))\n\(String(describing: self.myFollowersUsersArray[2].reputation!))"
+                        }
+                    case 1:
+                        if self.myFollowingUsersArray.indices.contains(0) {
+                            self.topUserLabel.text = "\(String(describing: self.myFollowingUsersArray[0].username!))\n\(String(describing: self.myFollowingUsersArray[0].reputation!))"
+                        }
+                        if self.myFollowingUsersArray.indices.contains(1) {
+                            self.secondUserLabel.text = "\(String(describing: self.myFollowingUsersArray[1].username!))\n\(String(describing: self.myFollowingUsersArray[1].reputation!))"
+                        }
+                        if self.myFollowingUsersArray.indices.contains(2) {
+                            self.thirdUserLabel.text = "\(String(describing: self.myFollowingUsersArray[2].username!))\n\(String(describing: self.myFollowingUsersArray[2].reputation!))"
+                        }
+                    default:
+                        break
+                    }
                 })
             })
             
+
         }
     }
     
@@ -72,7 +96,7 @@ class LeaderboardViewController: UIViewController, UITableViewDataSource, UITabl
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as! LeaderboardUserTableViewCell
         if folllowersFollowingSegmentedControl.selectedSegmentIndex == 0 {
-            cell.scoreLabel.text = String(describing: myFollowersUsersArray[indexPath.row].reputation)
+            cell.scoreLabel.text = String(describing: myFollowersUsersArray[indexPath.row].reputation!)
             cell.rankAndNameLabel.text = "\(indexPath.row + 1). \(String(describing: myFollowersUsersArray[indexPath.row].username!))"
         }
         return cell
