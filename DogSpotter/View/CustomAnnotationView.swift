d5231897dfa6dfce3ff82dbd50d48b69e8c3a89b
CodyPotter <enjoipotter@gmail.com>
2017-12-16 23:05:03 -0800

    working

---
+++ DogSpotter/View/CustomAnnotationView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomAnnotationView.swift
+//  DogSpotter
+//
+//  Created by Cody Potter on 9/20/17.
+//  Copyright © 2017 Cody Potter. All rights reserved.
+//
+
+import MapKit
+
+class CustomAnnotationView: MKAnnotationView {
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        if (hitView != nil) {
+            self.superview?.bringSubview(toFront: self)
+        }
+        return hitView
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let rect = self.bounds
+        var isInside: Bool = rect.contains(point)
+        if(!isInside) {
+            for view in self.subviews {
+                isInside = view.frame.contains(point)
+                if isInside {
+                    break
+                }
+            }
+        }
+        return isInside;
+    }
+}
